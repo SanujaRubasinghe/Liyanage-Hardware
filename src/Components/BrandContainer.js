8cb8e63405880ef79f188f63583a0640d1c885f3
Sanuja Rubasinghe <sanurubasinghe1234@gmail.com>
2025-02-15 17:55:15 +0530

    fix brand container

--- src/Components/BrandContainer.js
+++ src/Components/BrandContainer.js
@@ -3,7 +3,15 @@ import './BrandContainer.css'
 function BrandContainer() {
     const brandLogos = [
         {id: 1, image: '/images/b1.png', alt: 'messi'},
-        {id: 2, image: '/images/b2.png', alt: 'messi'}
+        {id: 2, image: '/images/b1.png', alt: 'messi'},
+        {id: 3, image: '/images/b1.png', alt: 'messi'},
+        {id: 4, image: '/images/b1.png', alt: 'messi'},
+        {id: 5, image: '/images/b1.png', alt: 'messi'},
+        {id: 6, image: '/images/b1.png', alt: 'messi'},
+        {id: 7, image: '/images/b1.png', alt: 'messi'},
+        {id: 8, image: '/images/b1.png', alt: 'messi'},
+        {id: 9, image: '/images/b1.png', alt: 'messi'},
+        {id: 10, image: '/images/b2.png', alt: 'messi'}
     ]
     return(
         <div className="brand-container">

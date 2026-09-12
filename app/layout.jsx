5d070eb5d76e86ab2decc08cce2e17f739d555ea
Yasanga32 <yasangashanuka33@gmail.com>
2026-09-12 20:29:55 +0530

    home section updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -28,7 +28,7 @@ export const viewport = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/logoicon.png" type="image/png" />
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
